bdd44e008d2acf17450dfaeadc1c9563bfac11fd
Andreysm1988 <48116197+Andreysm1988@users.noreply.github.com>
2019-04-16 15:11:17 +0300

    Тесты для формы логина

---
+++ Homework4/test_login_page_pass.py
@@ -0,0 +1,13 @@
+from selenium.webdriver.common.by import By
+
+
+def test_login_fail(driver, request):
+    url = '/opencart/admin/'
+    driver.get("".join([request.config.getoption("--address"), url]))
+    driver.find_element(By.ID, "input-username").send_keys("admin")
+    driver.find_element(By.ID, "input-password").send_keys("admin")
+    driver.find_element(By.CLASS_NAME, "btn.btn-primary").click()
+    warning = driver.find_element(By.CLASS_NAME, "close")
+    #warning_text = warning.text.rstrip("\n*'")
+    assert warning
+    #assert warning_text == "No match for Username and/or Password."
