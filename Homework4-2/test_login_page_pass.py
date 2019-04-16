7e1e2ff2813a33e0d13d749f56ffc144f2b73b5f
Andreysm1988 <48116197+Andreysm1988@users.noreply.github.com>
2019-04-16 15:28:20 +0300

    Add files via upload

---
+++ Homework4-2/test_login_page_pass.py
@@ -0,0 +1,11 @@
+from selenium.webdriver.common.by import By
+
+
+def test_login_pass(driver, request):
+    url = '/opencart/admin/'
+    driver.get("".join([request.config.getoption("--address"), url]))
+    driver.find_element(By.ID, "input-username").send_keys("admin")
+    driver.find_element(By.ID, "input-password").send_keys("admin")
+    driver.find_element(By.CLASS_NAME, "btn.btn-primary").click()
+    warning = driver.find_element(By.CLASS_NAME, "close")
+    assert warning
